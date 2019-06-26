06aec4acb90cb32abc25dafa461e8f01d95db97e
CodeDek <codedek@gmail.com>
2019-06-26 18:21:57 -0400

    New: added UnShareViewModel

---
+++ src/FolderSharer/ViewModels/UnShareViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FolderSharer.ViewModels
+{
+    public sealed class UnShareViewModel
+    {
+    }
+}
